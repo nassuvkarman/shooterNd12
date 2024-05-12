64c3e1d5e807d6320e238fedcc32f94a445af5ed
marian <vcesnc@gmail.com>
2024-05-12 13:10:36 +0300

    kvm igfi

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ SIZE = (WIDTH, HEIGHT)
 FPS = 60
 lost = 0
 score = 0
+monsters_num = 0
 window = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 background = pygame.transform.scale(
@@ -17,6 +18,7 @@ pygame.mixer.init()
 pygame.mixer.music.load("space.ogg")
 pygame.mixer.music.play()
 pygame.font.init()
+fire_sfx = pygame.mixer.Sound("fire.ogg")
 font_big = pygame.font.Font(None, 70)
 font_medium = pygame.font.Font(None, 35)
 font_small = pygame.font.Font(None, 15)
@@ -25,7 +27,7 @@ class GameSprite(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.transform.scale(
             pygame.image.load(image),
-            (65,65)
+            size
         )
         self.rect = self.image.get_rect()
         self.rect.center = coords
@@ -47,27 +49,51 @@ class Player(GameSprite):
             else:
                 self.rect.x = WIDTH
     def fire(self):
-        ...
+            new_bullet = Bullet("bullet.png",(self.rect.centerx, self.rect.top), 
+                                5, 
+                                (5,10))
+            bullets.add(new_bullet)
+            fire_sfx.play()
 class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
         if self.rect.bottom >= HEIGHT:
             self.rect.y = 0
+            global lost
+            lost += 1
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y -= self.speed 
+        if self.rect.bottom <= 0:
+            self.kill()
 
 player = Player("rocket.png", (WIDTH/2, HEIGHT-50), 5, (75,100))
 test_enemy = Enemy("ufo.png", (randint(50, WIDTH-50), 0), 5, (75,50))
+monsters = pygame.sprite.Group()
+
+for i in range(monsters_num):
+    new_enemy = Enemy("ufo.png", (randint(50,WIDTH-50),0), randint(2,8), (75,50))
+    monsters.add(new_enemy)
+bullets = pygame.sprite.Group()
 game = True
 finish = False
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.fire()
     if not finish:
         window.blit(background, (0,0))
         player.update()
         player.reset()
         test_enemy.update()
         test_enemy.reset()
+        monsters.update()
+        monsters.draw(window)
+        bullets.update()
+        bullets.draw(window)
 
         text_lost = font_medium.render("Пропущено: " + str(lost),
                                        True,
